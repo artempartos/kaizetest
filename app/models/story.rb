61ad5dc4e91a4ef5c66783b04d6c0fff746bef83
Partos <partos0511@gmail.com>
2012-10-16 13:05:32 +0400

    Per page removed from model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,16 +1,13 @@
 class Story < ActiveRecord::Base
   attr_accessible :deadline, :description, :performer_id, :title, :creator_id
-  #attr_accessor :state
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id, :inverse_of => :stories
   belongs_to :performer, :class_name => "User", :foreign_key => :performer_id
   has_many :story_comments
-
   validates :title, :presence => true
   validates :description,  :presence => true
   validates :creator_id, :presence => true
   validates :performer_id, :presence => true
-  Story.per_page= 10
-    state_machine :state, :initial => :new do
+  state_machine :state, :initial => :new do
 
     event :start do
       transition [:rejected, :new] => :started
