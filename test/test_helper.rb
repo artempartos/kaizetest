cb2901dce072c77857accfa8e88625df35e65572
Partos <partos0511@gmail.com>
2012-10-16 12:58:55 +0400

    Test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,22 @@
 require 'simplecov'
 SimpleCov.start('rails') if ENV["COVERAGE"]
 ENV["RAILS_ENV"] = "test"
+#require 'turn'
+#require 'factory_girl_rails'
 require File.expand_path('../../config/environment', __FILE__)
-#require 'rails/test_help'
+require 'rails/test_help'
 #Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
 #require "test/unit"
 #require "mocha"
+#require "minitest_helper"
 
 class ActiveSupport::TestCase
 
   include AuthHelper
-  include FactoryGirl::Syntax::Methods
-
+  include AccessHelper
+  require 'factory_girl'
 
   FactoryGirl.reload
-  include FactoryHelper
+  include FactoryGirl::Syntax::Methods
+  #include FactoryHelper
 end
